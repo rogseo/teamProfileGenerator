9840ea039531d5d095d69ba341b9d20550f41a5b
rogseo <rogseo@gmail.com>
2022-10-21 21:31:20 -0500

    file added

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer=require('inquirer');
+const question=[{
+    type: 'input',
+    message: `What is your team manager's name?`,
+    name: ,
+  },
+  {
+    type: 'input',
+    message: 'What languages do you know',
+    name: 'languages',
+    choices :['Korean','English','Spanish'],
+  },
+  {
+    type: 'input',
+    message: 'What is your preferred method of communication?',
+    name: 'method',
+  }];
+
+inquirer.propmt
+
