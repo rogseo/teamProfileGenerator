be097f29be632560990391e62dea55f6d0a67b90
rogseo <rogseo@gmail.com>
2022-10-22 23:56:09 -0500

    load each profile to generateHTML.js

--- index.js
+++ index.js
@@ -4,97 +4,60 @@ const Manager=require("./lib/Manager");
 const Engineer=require("./lib/Engineer");
 const fs=require("fs");
 const generateHTML=require("./lib/generateHTML");
-// Employee needed to be required?
 
-const managerQuestion=[{
-    type: 'input',
-    message: `What is your team manager's name?`,
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: `What is your team manager's ID`,
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: `What is your team manager's email`,
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: `What is your team manager's office number`,
-    name: 'officeNum',
-  },
-  {
-    type: 'list',
-    message: `which type of team member would you like to add?`,
-    choices:['Engineer','Intern',`I don't want to add anymore`],
-    name: 'type',
-}];
 
-const question={
-    type: 'list',
-    message: `which type of team member would you like to add?`,
-    choices:['Engineer','Intern',`I don't want to add anymore`],
-    name: 'type',
+
+const promptQuestion = (type)=>{
+    return [{
+        type: 'input',
+        message: `What is your ${type}'s name?`,
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: `What is your ${type}'s ID`,
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: `What is your ${type}'s email`,
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: `What is your ${type}'s office number`,
+        name: 'officeNum',
+        when() {
+            return type === 'manager'
+        }
+      },
+      {
+        type: 'input',
+        message: `What is your team ${type}'s github`,
+        name: 'github',
+        when() {
+            return type === 'engineer'
+        }
+      },
+      {
+        type: 'input',
+        message: `What is your team ${type}'s school`,
+        name: 'school',
+        when() {
+            return type === 'intern'
+        }
+      },
+      {
+        type: 'list',
+        message: `which type of team member would you like to add?`,
+        choices:['Engineer','Intern',`I don't want to add anymore`],
+        name: 'type',
+    },]
+
 }
 
-const engineerQuestion=[{
-    type: 'input',
-    message: `What is your team Engineer's name?`,
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: `What is your team engineer's ID`,
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: `What is your team engineer's email`,
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: `What is your team engineer's github`,
-    name: 'github',
-  },
-  {
-    type: 'list',
-    message: `which type of team member would you like to add?`,
-    choices:['Engineer','Intern',`I don't want to add anymore`],
-    name: 'type',
-}];
 
-const internQuestion=[{
-    type: 'input',
-    message: `What is your team intern's name?`,
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: `What is your team intern's ID`,
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: `What is your team intern's email`,
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: `What is your team intern's school`,
-    name: 'school',
-  },
-  {
-    type: 'list',
-    message: `which type of team member would you like to add?`,
-    choices:['Engineer','Intern',`I don't want to add anymore`],
-    name: 'type',
-}];
-
-  // TODO: Create a function to write README file
+// TODO: Create a function to write html file
 function writeToFile(fileName, markdown) {
     fs.writeFile(fileName, markdown, (err) =>
     err ? console.error(err) : console.log('Success!')
@@ -102,38 +65,33 @@ function writeToFile(fileName, markdown) {
 
 
 
-const employee=[];
+const employee=[]; // array of employee
 async function init() {  
     // Prompt Inquirer questions
-    var responses = await inquirer.prompt(managerQuestion);
-    //create class manager
-    //const manager=new Manager(responses.name,responses.id,responses.email,responses.officeNum);
+    var responses = await inquirer.prompt(promptQuestion('manager'));
     employee.push(new Manager(responses.name,responses.id,responses.email,responses.officeNum));
   
 
     while(responses.type!==`I don't want to add anymore`){
         if(responses.type==='Engineer'){
-            responses =await inquirer.prompt(engineerQuestion);
+            responses =await inquirer.prompt(promptQuestion('engineer'));
             employee.push(new Engineer(responses.name,responses.id,responses.email,responses.github));
-            console.log('engineer',responses);
 
         }
         else{//if Intern
-            responses =await inquirer.prompt(internQuestion);
+            responses =await inquirer.prompt(promptQuestion('intern'));
             employee.push(new Intern(responses.name,responses.id,responses.email,responses.school));
-            console.log('engineer',responses);
         }
 
     }
     
   
-
     console.log("Generating your HTML...")
     const html = generateHTML(employee);
     console.log(html);
 
   // Write markdown to file
-  await writeToFile('team.html', html);
+    await writeToFile('team.html', html);
   
   }
   
